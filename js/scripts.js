f72747c22f097bb9d7dcad04ffca031e27ba6f8e
gandhiv87 <vrushankgandhi@yahoo.com>
2022-06-19 23:35:27 -0500

    Update scripts.js
    Created pokemonList and added pokemon details to the list

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+let pokemonList [
+    {name: 'Bulbasur', height: '2', types: ['grass', 'poison']},
+    {name: 'Charmander', height: '2', types: ['fire']},
+    {name: 'Squirtle', height: '1', types: ['water']},
+    {name: 'Pikachu', height: '1', types: ['electric']},
+    {name: 'Gengar', height: '5', types: ['ghost', 'poison']},
+    {name: 'Dragonite', height: '7', types: ['dragon', 'flying']},
+];
