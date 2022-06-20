4484a8adee82bba6aa16d25f25d24df79ddc16b2
gandhiv87 <vrushankgandhi@yahoo.com>
2022-06-19 23:59:44 -0500

    Update scripts.js
    corrected the height according to the pokemon.com

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 let pokemonList [
-    {name: 'Bulbasur', height: '2', types: ['grass', 'poison']},
-    {name: 'Charmander', height: '2', types: ['fire']},
-    {name: 'Squirtle', height: '1', types: ['water']},
-    {name: 'Pikachu', height: '1', types: ['electric']},
-    {name: 'Gengar', height: '5', types: ['ghost', 'poison']},
-    {name: 'Dragonite', height: '7', types: ['dragon', 'flying']},
+    {name: 'Bulbasur', height: '2.04', types: ['grass', 'poison']},
+    {name: 'Charmander', height: '2.00', types: ['fire']},
+    {name: 'Squirtle', height: '1.08', types: ['water']},
+    {name: 'Pikachu', height: '1.04', types: ['electric']},
+    {name: 'Gengar', height: '4.11', types: ['ghost', 'poison']},
+    {name: 'Dragonite', height: '7.03', types: ['dragon', 'flying']},
 ];
