415bd44901a66e17942e741db0f556eaf628588f
gandhiv87 <vrushankgandhi@yahoo.com>
2022-06-20 00:17:21 -0500

    Update scripts.js
    Added for loop to the script that will display pokemon details to the main

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
-let pokemonList [
-    {name: 'Bulbasur', height: '2.04', types: ['grass', 'poison']},
-    {name: 'Charmander', height: '2.00', types: ['fire']},
-    {name: 'Squirtle', height: '1.08', types: ['water']},
-    {name: 'Pikachu', height: '1.04', types: ['electric']},
-    {name: 'Gengar', height: '4.11', types: ['ghost', 'poison']},
-    {name: 'Dragonite', height: '7.03', types: ['dragon', 'flying']},
+let pokemonList = [
+  {name: 'Bulbasur', height: '2.04', types: ['grass', 'poison']},
+  {name: 'Charmander', height: '2.00', types: ['fire']},
+  {name: 'Squirtle', height: '1.08', types: ['water']},
+  {name: 'Pikachu', height: '1.04', types: ['electric']},
+  {name: 'Gengar', height: '4.11', types: ['ghost', 'poison']},
+  {name: 'Dragonite', height: '7.03', types: ['dragon', 'flying']},
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name + 's ' + 'height is ' + pokemonList[i].height + '! ');
+}
