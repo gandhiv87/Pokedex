eb56360dc839f0e3784ea635d594555c28fc17d4
gandhiv87 <vrushankgandhi@yahoo.com>
2022-06-24 00:35:25 -0500

    Update scripts.js
    fixed syntax error

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 let pokemonList = [
-  {name: 'Bulbasur', height: '2.04', types: ['grass', 'poison']},
-  {name: 'Charmander', height: '2', types: ['fire']},
-  {name: 'Gengar', height: '4.11', types: ['ghost', 'poison']},
-  {name: 'Squirtle', height: '1.08', types: ['water']},
-  {name: 'Pikachu', height: '1.04', types: ['electric']},
-  {name: 'Dragonite', height: '7.03', types: ['dragon', 'flying']},
+  {name: 'Bulbasur', height: 2.04, types: ['grass', 'poison']},
+  {name: 'Charmander', height: 2, types: ['fire']},
+  {name: 'Gengar', height: 4.11, types: ['ghost', 'poison']},
+  {name: 'Squirtle', height: 1.08, types: ['water']},
+  {name: 'Pikachu', height: 1.04, types: ['electric']},
+  {name: 'Dragonite', height: 7.03, types: ['dragon', 'flying']},
 ];
 
 for (let i = 0; i < pokemonList.length; i++)
