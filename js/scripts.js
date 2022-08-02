00079f470918c8b711e750af9b0ae7bc850f9e69
gandhiv87 <vrushankgandhi@yahoo.com>
2022-08-01 23:45:33 -0500

    Update scripts.js
    IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,29 @@
-let pokemonList = [
-  {name: 'Bulbasur', height: 2.04, types: ['grass', 'poison']},
-  {name: 'Charmander', height: 2, types: ['fire']},
-  {name: 'Gengar', height: 4.11, types: ['ghost', 'poison']},
-  {name: 'Squirtle', height: 1.08, types: ['water']},
-  {name: 'Pikachu', height: 1.04, types: ['electric']},
-  {name: 'Dragonite', height: 7.03, types: ['dragon', 'flying']},
-];
+let pokemonRepository = (function() {
+  let pokemonList = [
+    {name: 'Bulbasur', height: 2.04, types: ['grass', 'poison']},
+    {name: 'Charmander', height: 2, types: ['fire']},
+    {name: 'Gengar', height: 4.11, types: ['ghost', 'poison']},
+    {name: 'Squirtle', height: 1.08, types: ['water']},
+    {name: 'Pikachu', height: 1.04, types: ['electric']},
+    {name: 'Dragonite', height: 7.03, types: ['dragon', 'flying']},
+  ];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+
+})();
+
+let pokemonList= pokemonRepository.getAll();
 
 pokemonList.forEach(function(pokemon)
 {
